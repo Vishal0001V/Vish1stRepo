897a58ecdaef9ce30496ff4dc8b5d5f7304e5a3f
Vishal0001V <vishalkailasbhume@gmail.com>
2024-11-18 20:00:07 +0530

    Update Ex1.java

--- GitProj/src/Demo/Ex1.java
+++ GitProj/src/Demo/Ex1.java
@@ -6,5 +6,5 @@ int a=10;
 	int b=20;
 	System.out.println(a+b);
 	//this is how we can  use git to code from of the the n number of the developers
-	//this code and the git repositories perpose is just to get more familiar with the gittools and how to use it
+	//this code and the git repositories perpose is just to get more familiar with the gittools and how to use it so this is the all about this packagess
 }
