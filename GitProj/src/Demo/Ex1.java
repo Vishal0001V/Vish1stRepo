0a75a97b4c7270a1fc8e63b0d890082bacc2fa99
Administrator <Administrator@DEKSTOPVishal>
2024-11-18 20:04:13 +0530

    commit and push

--- GitProj/src/Demo/Ex1.java
+++ GitProj/src/Demo/Ex1.java
@@ -1,10 +1,11 @@
 package Demo;
-      
+
 public class Ex1 {
 public static void main(String[] args) {
-int a=10;		
+int a=10;
 	int b=20;
 	System.out.println(a+b);
 	//this is how we can  use git to code from of the the n number of the developers
-	//this code and the git repositories perpose is just to get more familiar with the gittools and how to use it
+	//comitt and push
+}
 }
