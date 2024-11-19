2c839a65ef5e173fe42c1ad9761e60b0bfbd80b5
Vishal0001V <vishalkailasbhume@gmail.com>
2024-11-19 18:19:24 +0530

    Update Ex1.java
    kjhgfd

--- GitProj/src/Demo/Ex1.java
+++ GitProj/src/Demo/Ex1.java
@@ -5,8 +5,7 @@ public static void main(String[] args) {
 int a=10;
 	int b=20;
 	System.out.println(a+b);
-	//just try again and again
-	//lines from of  the eclipse
+	
 	//again a change for the push
 	System.out.println("");
 		System.out.println("");
